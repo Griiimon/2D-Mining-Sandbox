bc5d638c13aaf48fff6177e9031c7175af45f81e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-09 15:04:15 +0200

    added tags

--- scripts/item.gd
+++ scripts/item.gd
@@ -4,6 +4,8 @@ extends Resource
 @export var name: String
 @export var texture: Texture2D
 @export var can_stack: bool= true
+@export var tags: Array[Tag]
+
 
 @export_category("Misc")
 @export var fuel_value: float= 0
