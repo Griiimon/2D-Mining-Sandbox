ae0bc49cb589fe909819c90e343894644402a976
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-09 13:47:54 +0200

    added furnace recipe resource

--- scripts/data_manager.gd
+++ scripts/data_manager.gd
@@ -1,12 +1,18 @@
 extends Node
 
 var blocks: Array[Block]
+var furnace_recipes: Array[FurnaceRecipe]
 
-# load all blocks from the resource folder
+
 func _init():
-	var folder: String= "res://resources/blocks/"
+	load_resource_folder_into_array("res://resources/blocks", blocks)
+	load_resource_folder_into_array("res://resources/recipes/furnace", furnace_recipes)
+
+
+static func load_resource_folder_into_array(folder: String, array: Array):
+	folder+= "/"
 	var dir:= DirAccess.open(folder)
 	
 	for file in dir.get_files():
 		var item= load(folder + file)
-		blocks.append(item)
+		array.append(item)
---
+++ scripts/furnace_recipe.gd
@@ -0,0 +1,7 @@
+class_name FurnaceRecipe
+extends Resource
+
+@export var ingredient: Item
+@export var product: Item
+@export var required_fuel: float
+@export var duration: float
