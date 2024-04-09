316c52c87bc52ae9c30dd6f7a1739f9128abbd21
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-09 13:57:00 +0200

    spawn furnace

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,6 +4,12 @@ extends Node
 @onready var world: World = $TileMap
 
 
+
+
+func _ready():
+	world.spawn_block_entity(Vector2i(2, 4), load("res://scenes/block entities/furnace.tscn"))
+
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
