e16d051080529f9e4c174759d9f28765d40d81a9
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-09 13:46:05 +0200

    added furnace

---
+++ scripts/block entities/furnace.gd
@@ -0,0 +1,65 @@
+class_name Furnace
+extends BaseBlockEntity
+
+@export var default_texture: Texture2D
+@export var working_texture: Texture2D
+
+
+var fuel: float
+var ore_type: Item
+var ore_amount: int
+
+var product_type: Item
+var product_amount: int
+
+var is_burning: bool
+var ticks_to_finish: float
+
+
+func _ready():
+	super()
+	sprite.texture= default_texture
+
+
+func tick(world: World):
+	if is_burning:
+		ticks_to_finish-= 1
+		if ticks_to_finish == 0:
+			product_amount+= 1
+			is_burning= false
+		else:
+			return
+			
+	if ore_type and ore_amount > 0:
+		var recipe: FurnaceRecipe= DataManager.find_furnace_recipe_for(ore_type)
+		
+		if fuel < recipe.required_fuel:
+			return
+		if product_type and product_type != recipe.product:
+			return
+		
+		product_type= recipe.product
+		fuel-= recipe.required_fuel
+		ticks_to_finish= recipe.duration * World.ENTITY_TICKS
+		is_burning= true
+
+
+func interact(player: Player):
+	if player.is_current_inventory_slot_empty() or player.get_current_inventory_item().item == product_type:
+		if product_amount > 0:
+			player.inventory.add_item_to_slot(player.get_current_inventory_slot(), product_type)
+			product_amount-= 1
+	else:
+		var inv_item: InventoryItem= player.get_current_inventory_item()
+		if inv_item.item.fuel_value > 0:
+			NodeDebugger.msg(self, "adding fuel")
+			fuel+= inv_item.item.fuel_value
+			player.inventory.sub_item(inv_item)
+		else:
+			var recipe: FurnaceRecipe= DataManager.find_furnace_recipe_for(inv_item.item)
+			if recipe:
+				if ore_type == null or ore_type == inv_item.item:
+					NodeDebugger.msg(self, "adding ore")
+					ore_amount+= 1
+					player.inventory.sub_item(inv_item)
+
