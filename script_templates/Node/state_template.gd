29af6eea08017e873274c5d06d8a7e62ae511034
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-13 08:37:31 +0200

    Create state_template.gd

---
+++ script_templates/Node/state_template.gd
@@ -0,0 +1,18 @@
+extends StateMachineState
+
+
+
+func on_enter() -> void:
+	pass
+
+
+func on_process(_delta: float) -> void:
+	pass
+
+
+func on_physics_process(_delta: float) -> void:
+	pass
+
+
+func on_exit() -> void:
+	pass
