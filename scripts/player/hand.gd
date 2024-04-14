411cf39166d727b42b69a1ad5d572163bb222059
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-14 14:56:47 +0200

    add virtual projectile thrower

--- scripts/player/hand.gd
+++ scripts/player/hand.gd
@@ -1,6 +1,12 @@
 extends Node2D
 class_name Hand
 
+@export var player: Player
+
+
+func _ready():
+	assert(player)
+
 
 func set_attack_state(b: bool):
 	assert(get_child_count() == 1)
---
+++ scripts/projectiles/virtual_projectile_thrower.gd
@@ -0,0 +1,23 @@
+class_name VirtualProjectileThrower
+extends HandItemObject
+
+var hand_object: Node2D
+
+
+func on_equip():
+	var projectile: Projectile= type.scene.instantiate()
+	var hand: Hand= get_parent()
+	hand.add_child(projectile)
+	hand_object= projectile.visual
+	# reparent visual representation of projectile to virtual thrower
+	hand_object.reparent(hand.player.get_hand_item())
+	projectile.queue_free()
+
+
+func release_charge(total_charge: float, primary: bool):
+	var hand: Hand= get_parent()
+	var projectile: Projectile= type.scene.instantiate()
+	projectile.position= hand.global_position
+	hand_object.queue_free()
+	Global.game.add_child(projectile)
+	projectile.shoot(hand.player.get_look_direction() * total_charge * 500)
