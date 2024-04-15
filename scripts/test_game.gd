895ad738308387343024c20055e82c761283f36f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-15 15:17:58 +0200

    water

--- scripts/test_game.gd
+++ scripts/test_game.gd
@@ -4,5 +4,11 @@ extends Game
 func _ready():
 	super()
 	world.spawn_block_entity(Vector2i(-7, 6), load("res://scenes/block entities/furnace.tscn"))
-	for x in 4:
-		world.set_block(Vector2i(x, 6), load("res://resources/blocks/water_block.tres"))
+
+
+func post_init():
+		var y:= 8
+		for x in 8:
+			world.set_block(Vector2i(x, y), load("res://resources/blocks/water_block.tres"))
+			if x > 3:
+				world.set_block(Vector2i(x, y + 1), load("res://resources/blocks/water_block.tres"))
