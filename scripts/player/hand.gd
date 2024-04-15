db82ec71f3f7587d038d35981e00b18c82a24a07
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-15 04:30:09 +0200

    hand connector

--- scripts/player/hand.gd
+++ scripts/player/hand.gd
@@ -3,11 +3,22 @@ class_name Hand
 
 @export var player: Player
 
+@onready var connector = $Connector
+
+
 
 func _ready():
 	assert(player)
 
 
 func set_attack_state(b: bool):
-	assert(get_child_count() == 1)
-	(get_child(0) as HandItemObject).set_attack_state(b)
+	assert(has_hand_object())
+	get_hand_object().set_attack_state(b)
+
+
+func has_hand_object()-> bool:
+	return connector.get_child_count() > 0
+
+
+func get_hand_object()-> HandItemObject:
+	return connector.get_child(0)
