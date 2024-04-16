c0a3701226a5f8ce677cb44fc535d500b94f16b2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-16 02:38:04 +0200

    tile detector

---
+++ scripts/components/tile_detector.gd
@@ -0,0 +1,16 @@
+class_name TileDetector
+extends Node2D
+
+@export var active: bool= true
+
+var current_tile: Vector2i
+var current_block: Block
+
+
+func _physics_process(delta):
+	current_tile= Global.game.world.get_tile(global_position)
+	current_block= Global.game.world.get_block(current_tile)
+
+
+func is_in_fluid()-> bool:
+	return current_block and current_block.is_fluid
