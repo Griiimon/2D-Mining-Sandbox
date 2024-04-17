38f1f8e81c0df702ad2dae7cd3809d6d53d14682
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-18 01:40:43 +0200

    fix end attack animation

--- scripts/player/hand_item_object.gd
+++ scripts/player/hand_item_object.gd
@@ -13,8 +13,8 @@ func set_attack_state(b: bool):
 	if b:
 		if action_player.has_animation(START_ATTACK_ANIMATION_ACTION):
 			action_player.play(START_ATTACK_ANIMATION_ACTION)
-		elif action_player.has_animation(END_ATTACK_ANIMATION_ACTION):
-			action_player.play(END_ATTACK_ANIMATION_ACTION)
+	elif action_player.has_animation(END_ATTACK_ANIMATION_ACTION):
+		action_player.play(END_ATTACK_ANIMATION_ACTION)
 
 
 func release_charge(_total_charge: float, _primary: bool):
