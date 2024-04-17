77afeae23ed540da91a80d653e72cc11baaa7f29
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-17 13:32:15 +0200

    Update debug_component.gd

--- scripts/components/debug_component.gd
+++ scripts/components/debug_component.gd
@@ -7,7 +7,22 @@ enum Type { DEFAULT, WARNING, ERROR, CRITICAL_ERROR }
 @export var max_detail_level: int= 0
 @export var prefix: String
 
+
 func debug_message(text: String, detail_level: int, type: Type):
 	if not enabled: return
+	if detail_level > max_detail_level: return
 	
-	print((prefix +": " if prefix else "") + text)
+	var final_text: String= (prefix +": " if prefix else "") + text
+
+	print(final_text)
+	
+	match type:
+		Type.WARNING:
+			push_warning(final_text)
+		Type.ERROR:
+			push_error(final_text)
+		Type.CRITICAL_ERROR:
+			push_error(final_text)
+			assert(false)
+			
+		
