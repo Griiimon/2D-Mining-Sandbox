6bfe45de884bff6c9205f89fcbf6a5bbfda2784e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-18 17:39:25 +0200

    project restructure

--- scripts/autoloads singletons/data_manager.gd
+++ scripts/autoloads singletons/data_manager.gd
@@ -1,35 +1,42 @@
-@tool
+#@tool
 extends Node
 
-const TILE_SET_PATH= "res://resources/tile_set.tres"
-const ORIG_TILE_SET_PATH= "res://resources/orig_tile_set.tres"
+#const TILE_SET_PATH= "res://resources/tile_set.tres"
+#const ORIG_TILE_SET_PATH= "res://resources/orig_tile_set.tres"
+
+@export var tile_set: TileSet
+@export var orig_tile_set: TileSet
+@export_dir var blocks_path: String
+@export var blocks_suffix: String
+@export_dir var furnace_recipe_path: String
+@export_dir var mobs_path: String
+@export var mobs_suffix: String
+@export var fluid_library: FluidLibrary
+
 
 var blocks: Array[Block]
 var blocks_lookup: Dictionary
 
 var furnace_recipes: Dictionary
 
-var fluid_library: FluidLibrary
 
 var mobs: Array[MobDefinition]
 
-var tile_set: TileSet
+#var tile_set: TileSet
 
 
 
 func _init():
-	load_resource_folder_into_array("res://resources/blocks", blocks)
+	load_resource_folder_into_array(blocks_path, blocks, blocks_suffix)
 	
 	for i in len(blocks):
 		blocks_lookup[blocks[i]]= i
 
 	if Engine.is_editor_hint(): return
 
-	load_resource_folder_into_dictionary("res://resources/recipes/furnace", furnace_recipes, "ingredient")
+	load_resource_folder_into_dictionary(furnace_recipe_path, furnace_recipes, "ingredient")
 
-	fluid_library= load("res://resources/libraries/fluid_library.tres")
-
-	load_resource_folder_into_array("res://resources/mobs", mobs)
+	load_resource_folder_into_array(mobs_path, mobs, mobs_suffix)
 
 	late_init.call_deferred()
 
@@ -39,13 +46,16 @@ func late_init():
 	fluid_library.build()
 
 
-func load_resource_folder_into_array(folder: String, array: Array):
-	folder+= "/"
-	var dir:= DirAccess.open(folder)
-	
-	for file in dir.get_files():
-		var item= load(folder + file)
-		array.append(item)
+func load_resource_folder_into_array(folder: String, array: Array, suffix: String):
+	#folder+= "/"
+	#var dir:= DirAccess.open(folder)
+	#
+	#for file in dir.get_files():
+		#var item= load(folder + file)
+		#array.append(item)
+	for file in Utils.load_directory_recursively(folder):
+		if file.ends_with(suffix):
+			array.append(load(file))
 
 
 func load_resource_folder_into_dictionary(folder: String, dict: Dictionary, key: String):
--- scripts/autoloads singletons/utils.gd
+++ scripts/autoloads singletons/utils.gd
@@ -12,3 +12,17 @@ static func chance100(chance: float)-> bool:
 
 static func chance50()-> bool:
 	return chance100(50)
+
+
+static func load_directory_recursively(path: String)-> Array[String]:
+	var result: Array[String]= []
+	path+= "/"
+	var dir:= DirAccess.open(path)
+	
+	for sub_dir in dir.get_directories():
+		result.append_array(load_directory_recursively(path + sub_dir))
+	
+	for file in dir.get_files():
+		result.append(path + file)
+	
+	return result
