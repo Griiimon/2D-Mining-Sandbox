0208527ca6e0e70eee9208ee375cb741ad00c8f7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-19 10:12:04 +0200

    update

--- game/test_game.gd
+++ game/test_game.gd
@@ -3,11 +3,11 @@ extends Game
 
 func _ready():
 	super()
-	world.spawn_block_entity(Vector2i(-7, 6), load("res://scenes/block entities/furnace.tscn"))
+	world.spawn_block_entity(Vector2i(-7, 6), load("res://game/block entities/furnace/furnace.tscn"))
 
 
 func post_init():
-	var water_block: Block= load("res://resources/blocks/water_block.tres")
+	var water_block: Block= load("res://game/blocks/water blocks/water_block.tres")
 	var y:= 8
 	for x in 12:
 		world.delete_block(Vector2i(x, y - 1), false)
