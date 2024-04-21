7e171800aecdd3e4104862f3ffd76cc025c62d06
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-21 08:33:50 +0200

    deal solid entity damage

--- game/components/damage system/damage_area.gd
+++ game/components/damage system/damage_area.gd
@@ -32,18 +32,29 @@ func _on_area_entered(area):
 	hurtbox.receive_damage(damage)
 
 
+func _on_body_entered(body):
+	var health_component= body.get_node_or_null(Global.HEALTH_COMPONENT_NODE)
+	if health_component: 
+		health_component.receive_damage(damage)
+
 func set_enabled(b: bool):
 	if enabled != b:
 		enabled= b
 		if not collision_shape:
 			await ready
 		collision_shape.disabled= not enabled
+
 	if enabled:
+		# makes sure to trigger entered even if its already overlapping when enabled
 		for overlap in get_overlapping_areas():
-			# makes sure to trigger area_entered even if its already overlapping when enabled
 			_on_area_entered(overlap)
+		
+		if damage_solid_block_entity:
+			for overlap in get_overlapping_bodies():
+				_on_body_entered(overlap)
 
 
 func on_interval_timeout():
 	set_enabled(true)
 	set_enabled.call_deferred(false)
+
