10462c5c988c96275445daa5b38ab39c55f1b35f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-21 08:34:22 +0200

    remove export var has_collision

--- game/block entities/base_block_entity.gd
+++ game/block entities/base_block_entity.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 # size in tiles
 @export var size: Vector2i= Vector2i.ONE
-@export var has_collision: bool= false
+#@export var has_collision: bool= false
 @export var register_tick: bool= false
 
 @onready var sprite: Sprite2D = $Sprite2D
@@ -14,4 +14,4 @@ func _ready():
 	collision_shape.shape.size= size * World.TILE_SIZE
 	collision_shape.position= size * World.TILE_SIZE / 2
 	
-	collision_shape.disabled= not has_collision
+#	collision_shape.disabled= not has_collision
