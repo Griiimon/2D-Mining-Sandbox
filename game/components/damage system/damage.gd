0b43d40d2846b2636c89875eca6133ed2caac3f0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-22 06:31:18 +0200

    Update damage.gd

--- game/components/damage system/damage.gd
+++ game/components/damage system/damage.gd
@@ -1,9 +1,13 @@
 extends Resource
 class_name Damage
 
-enum Type { MELEE, EXPLOSION, ENVIRONMENT }
+enum Type { MELEE, EXPLOSION, ENVIRONMENT, FALL }
 
 
 @export var type: Type
-@export var value: int
+@export var value: float
 
+
+func _init(_value: float= 0, _type: Type= Type.MELEE):
+	type= _type
+	value= _value
