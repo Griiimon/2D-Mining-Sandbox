455f658cbf66b4cc7e49b90f516021e5307a2e9b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-23 17:17:25 +0200

    auto unregister on deletion

--- game/block entities/base_block_entity.gd
+++ game/block entities/base_block_entity.gd
@@ -3,7 +3,6 @@ extends StaticBody2D
 
 # size in tiles
 @export var size: Vector2i= Vector2i.ONE
-#@export var has_collision: bool= false
 @export var register_tick: bool= false
 
 @onready var sprite: Sprite2D = $Sprite2D
@@ -13,5 +12,7 @@ extends StaticBody2D
 func _ready():
 	collision_shape.shape.size= size * World.TILE_SIZE
 	collision_shape.position= size * World.TILE_SIZE / 2
-	
-#	collision_shape.disabled= not has_collision
+
+
+func _exit_tree():
+	Global.game.world.unregister_entity(self)
