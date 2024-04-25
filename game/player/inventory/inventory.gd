90b442f43d49e4af2a2e2060994b114443721c41
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-25 02:56:53 +0200

    Update inventory.gd

--- game/player/inventory/inventory.gd
+++ game/player/inventory/inventory.gd
@@ -7,6 +7,7 @@ var items: Array[InventoryItem]
 
 var update_callback: Callable
 
+var block_update_callback: bool= false
 
 
 func _init():
@@ -14,17 +15,34 @@ func _init():
 		items.append(InventoryItem.new())
 
 
-func add_item(inv_item: InventoryItem, amount: int= 1):
+func add_item(inv_item: InventoryItem, amount: int= 1, do_update: bool= true):
 	inv_item.amount+= amount
-	updated()
+	if do_update:
+		updated()
 
 
-func sub_item(inv_item: InventoryItem, amount: int= 1):
+func sub_item(inv_item: InventoryItem, amount: int= 1, do_update: bool= true):
 	assert(inv_item.amount >= amount)
 	inv_item.amount-= amount
-	if inv_item.amount == 0:
-		inv_item.item= null
-	updated()
+
+	if do_update:
+		updated()
+
+
+func sub(item_type: Item, amount: int= 1, do_update: bool= true):
+	var total: int= amount
+	for inv_item in items:
+		if inv_item.item == item_type:
+			var before: int= inv_item.amount
+			inv_item.amount= max(inv_item.amount - total, 0)
+			total-= before - inv_item.amount
+			
+			if total == 0:
+				if do_update:
+					updated()
+				return
+	
+	assert(false, "trying to substract non-existing item/too many items")
 
 
 func add_new_item(item: Item, amount: int= 1):
@@ -53,6 +71,31 @@ func add_item_to_slot(slot_idx: int, item: Item, amount: int= 1):
 	updated()
 
 
+func sub_ingredients(ingredients: Array[InventoryItem]):
+	for ingredient in ingredients:
+		sub(ingredient.item, ingredient.amount, false)
+	updated()
+
+
+func has_ingredients(ingredients: Array[InventoryItem])-> bool:
+	for ingredient in ingredients:
+		if not has(ingredient.item, ingredient.amount):
+			return false
+	return true
+
+
+func has(item_type: Item, amount: int= 0)-> bool:
+	var total: int= amount
+	for inv_item in items:
+		if inv_item.item == item_type:
+			if inv_item.amount >= total:
+				return true
+				
+			total-= inv_item.amount
+			
+	return false
+
+
 func find_empty_slot()-> int:
 	for i in SIZE:
 		if not items[i].item:
@@ -71,5 +114,5 @@ func clear_item(inv_item: InventoryItem):
 
 
 func updated():
-	if update_callback:
+	if update_callback and not block_update_callback:
 		update_callback.call()
