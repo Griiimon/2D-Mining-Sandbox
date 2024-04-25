322a283dc36317f3ca3976a8897f52411514d08e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-25 03:20:19 +0200

    Create dying_state.gd

---
+++ game/player/dying_state.gd
@@ -0,0 +1,12 @@
+extends BasePlayerState
+
+
+func on_enter():
+	player.init_death()
+
+	var tween= create_tween()
+	tween.tween_property(player, "rotation", deg_to_rad(-90 * player.body.scale.x), 2).set_ease(Tween.EASE_OUT)
+	await tween.finished
+	await get_tree().create_timer(2).timeout
+
+	player.queue_free()
