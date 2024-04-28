09c875ada622d512348ca686bba401f322a134e1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-28 15:37:09 +0200

    Update building_state.gd

--- game/player/state machine/building_state.gd
+++ game/player/state machine/building_state.gd
@@ -3,11 +3,17 @@ extends PlayerState
 signal build(scene, tile_pos)
 signal cancel
 
+enum Type { Entity, Block }
+
+var type: Type
 var entity_scene: PackedScene
 var ghost: BaseBlockEntity
+var block: Block
 
+var empty_tile: Vector2i: set= set_empty_tile
 
 func on_enter():
+	type= Type.Entity
 	entity_scene= load("res://game/block entities/furnace/furnace.tscn")
 	ghost= entity_scene.instantiate()
 	add_child(ghost)
@@ -15,7 +21,14 @@ func on_enter():
 
 
 func on_physics_process(delta: float):
-	if player.ray_cast.is_colliding() and is_raycast_hitting_terrain():
-		
+	if player.ray_cast.is_colliding():
+		#empty_tile= get_empty_tile()
+
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			build.emit()
+			build.emit(entity_scene, empty_tile)
+
+
+func set_empty_tile(tile_pos: Vector2i):
+	if tile_pos == empty_tile: return
+	
+	empty_tile= tile_pos
