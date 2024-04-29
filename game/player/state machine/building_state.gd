e0451171895ae7fac0ee222e6c18bc0d0e9c9ad8
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-04-29 20:38:21 +0200

    Update building_state.gd

--- game/player/state machine/building_state.gd
+++ game/player/state machine/building_state.gd
@@ -1,6 +1,7 @@
 extends PlayerState
 
-signal build(scene, tile_pos)
+signal build_entity(scene, tile_pos)
+signal build_block(block, alternate_id, tile_pos)
 signal cancel
 
 enum Type { Entity, Block }
@@ -9,47 +10,77 @@ var type: Type
 var entity_scene: PackedScene
 var ghost: BaseBlockEntity
 var ghost_orig_collision_layer: int
-var block: Block
 
-var empty_tile: Vector2i: set= set_empty_tile
+var block: Block
+var block_alternate_id: int
+
+var empty_tile: Vector2i
 
 
 
 func on_enter():
 	
+	init_block_entity(scene)
 	type= Type.Entity
 	entity_scene= load("res://game/block entities/furnace/furnace.tscn")
 	ghost= entity_scene.instantiate()
 	ghost_orig_collision_layer= ghost.collision_layer
 	ghost.collision_layer= 0
 	add_child(ghost)
-	ghost.modulate= Color.WHITE.lerp(Color.TRANSPARENT, 0.5).lerp(Color.RED, 0.25)
+	ghost.modulate= get_ghost_modulate_color(true)
 	ghost.top_level= true
 	ghost.hide()
 
 
 func on_physics_process(delta: float):
 	if player.ray_cast.is_colliding():
-		empty_tile= get_empty_tile()
+		var new_empty_tile= get_empty_tile()
+		if empty_tile == new_empty_tile: return
+		
+		empty_tile= new_empty_tile
 
 		if player.is_in_tile(empty_tile): return
 		
 		DebugHud.send("Empty Tile", str(empty_tile))
 		
-		if not player.get_world().can_spawn_block_entity_at(ghost, empty_tile):
-			return
+		var valid_pos= null
+		
+		# TODO remove duplicate positions ( if size.x/y == 1 )
+		var test_positions= [ empty_tile, empty_tile - Vector2i(ghost.size.x - 1, 0),\
+				empty_tile - Vector2i(0, ghost.size.y - 1), empty_tile - ghost.size - Vector2i.ONE]
+
+		for pos in test_positions:
+			if player.get_world().can_spawn_block_entity_at(ghost, pos):
+				valid_pos= pos
+				break
+		
+		if valid_pos == null: return
 		
 		ghost.show()
-		ghost.position= get_world().map_to_local(empty_tile) - Vector2.ONE * World.TILE_SIZE / 2
+		ghost.position= get_world().map_to_local(valid_pos) - Vector2.ONE * World.TILE_SIZE / 2
 		
 		DebugHud.send("Ghost Pos", str(ghost.position))
 		
 		
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			build.emit(entity_scene, empty_tile)
+			build_entity.emit(entity_scene, empty_tile)
 
 
-func set_empty_tile(tile_pos: Vector2i):
-	if tile_pos == empty_tile: return
+func init_block_entity(scene: PackedScene):
+	type= Type.Entity
+	ghost= scene.instantiate()
 	
-	empty_tile= tile_pos
+	ghost_orig_collision_layer= ghost.collision_layer
+	ghost.collision_layer= 0
+	add_child(ghost)
+	
+	ghost.modulate= get_ghost_modulate_color(true)
+	ghost.top_level= true
+	ghost.hide()
+
+
+func get_ghost_modulate_color(valid: bool)-> Color:
+	var color= Color.WHITE.lerp(Color.TRANSPARENT, 0.5)
+	if not valid:
+		color= color.lerp(Color.RED, 0.25)
+	return color
