ce01cdf39ae2167fb9585292c0249288e807590d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-03 19:34:13 +0200

    BlockEntityDefinition

--- game/block entities/base_block_entity.gd
+++ game/block entities/base_block_entity.gd
@@ -2,19 +2,17 @@ class_name BaseBlockEntity
 extends StaticBody2D
 
 
-@export var entity_name: String
-# size in tiles
-@export var size: Vector2i= Vector2i.ONE
-@export var register_tick: bool= false
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
+var type: BlockEntityDefinition
 var foundation: Array[Vector2i]
 
 
 
 func _ready():
+	assert(type)
 	collision_shape.shape.size= size * World.TILE_SIZE
 	collision_shape.position= size * World.TILE_SIZE / 2
 	
---
+++ game/block entities/block_entity_definition.gd
@@ -0,0 +1,9 @@
+extends MyNamedResource
+class_name BlockEntityDefinition
+
+
+@export var scene: PackedScene
+# size in tiles
+@export var size: Vector2i= Vector2i.ONE
+@export var register_tick: bool= false
+
