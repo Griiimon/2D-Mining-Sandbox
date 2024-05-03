9045780ec0cc8112248a3b128f464e529e2f3b5e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-03 13:04:32 +0200

    Update mob_spawner.gd

--- game/world/components/mob spawner/mob_spawner.gd
+++ game/world/components/mob spawner/mob_spawner.gd
@@ -23,20 +23,19 @@ func _on_timer_timeout():
 		safe_ctr+= 1
 
 	var mob_def: MobDefinition= DataManager.mobs.pick_random()
-	
 	var spawner: MobSpawnerDefinition= mob_def.spawner
 	
 	if spawner.can_spawn(tile, world):
 		var other_mob: BaseMob= get_world().get_closest_mob(tile)
 		if other_mob:
 			var dist: int= other_mob.distance_to_tile(tile)
-			if dist < mob_def.min_distance_to_other:
+			if dist < spawner.min_distance_to_other:
 				return
-			elif other_mob.type == mob_def and dist < mob_def.min_distance_to_same:
+			elif other_mob.type == mob_def and dist < spawner.min_distance_to_same:
 				return
 			
 			other_mob= get_world().get_closest_mob(tile, mob_def)
-			if dist < mob_def.min_distance_to_same:
+			if dist < spawner.min_distance_to_same:
 				return
 
 		
