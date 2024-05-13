8bf631c08775bb2a69b5e773e239d64ac6d92136
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-13 02:17:17 +0200

    Update base_scenario.gd

--- game/scenarios/base_scenario.gd
+++ game/scenarios/base_scenario.gd
@@ -9,9 +9,9 @@ class_name BaseScenario
 
 func pre_start():
 	for tile_pos in level_editor.get_used_cells(WorldChunk.TERRAIN_LAYER):
-		var block: Block= DataManager.get_block(level_editor.get_cell_source_id(tile_pos))
+		var block: Block= DataManager.get_block(level_editor.get_cell_source_id(WorldChunk.TERRAIN_LAYER, tile_pos))
 		if block and not block.is_air:
-			world.set_block(block, tile_pos, Block.get_state_from_alt(level_editor.get_cell_alternative_tile(tile_pos)))
+			world.set_block(block, tile_pos, Block.get_state_from_alt(level_editor.get_cell_alternative_tile(WorldChunk.TERRAIN_LAYER, tile_pos)))
 	
 	level_editor.queue_free()
 	return get_tree().process_frame
