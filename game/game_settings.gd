2fd667d7e537d1842388f22eeeefdd5dc2235113
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-13 23:27:41 +0200

    Update game_settings.gd

--- game/game_settings.gd
+++ game/game_settings.gd
@@ -4,4 +4,5 @@ extends Resource
 @export var world_seed: int
 @export var player_spawn: Vector2i
 @export var spawn_clearing_radius: int= 4
+@export var respawn_on_death: bool= true
 @export var player_loadout: PlayerLoadout
