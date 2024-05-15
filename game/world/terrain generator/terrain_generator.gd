cbd69897d14c15201f57c7b49c6cd8215a50e23d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-15 14:40:28 +0200

    removed instruction priority

--- game/world/terrain generator/terrain_generator.gd
+++ game/world/terrain generator/terrain_generator.gd
@@ -3,25 +3,11 @@ extends Resource
 
 @export var instructions: Array[TerrainGeneratorInstruction]
 
-var sorted_instructions: Array[TerrainGeneratorInstruction]
 
 
 func initialize():
-	sort_instructions()
-	for instruction in sorted_instructions:
+	for instruction in instructions:
 		instruction.initialize()
-
-
-func sort_instructions():
-	var tmp_instructions:= instructions.duplicate()
-	
-	while tmp_instructions:
-		var result: TerrainGeneratorInstruction
-		for instruction in tmp_instructions:
-			if not result or instruction.priority > result.priority:
-				result= instruction
-		sorted_instructions.append(result)
-		tmp_instructions.erase(result)
 
 
 func get_block_id(pos: Vector2i)-> int:
--- game/world/terrain generator/terrain_generator_instruction.gd
+++ game/world/terrain generator/terrain_generator_instruction.gd
@@ -1,7 +1,6 @@
 class_name TerrainGeneratorInstruction
 extends Resource
 
-@export var priority: int= 0
 @export var min_height: int= 0
 @export var max_height: int= 1024
 @export var distribution: TerrainBlockDistribution
