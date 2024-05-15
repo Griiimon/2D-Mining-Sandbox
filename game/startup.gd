8c3d278a8b590f16cdaed76c359a91dc3cff66da
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-15 02:08:10 +0200

    move startup routine to GameManager

--- game/startup.gd
+++ game/startup.gd
@@ -1,16 +1,4 @@
 extends Node
 
-@export var skip_main_menu: bool= false
-@export var skip_to_scene: PackedScene
-@export var main_menu: PackedScene
-
-
-
 func _ready():
-	assert(main_menu)
-	assert(not skip_main_menu or skip_to_scene != null)
-	
-	if skip_main_menu:
-		GameManager.run_game(skip_to_scene)
-	else:
-		get_tree().change_scene_to_packed(main_menu)
+	GameManager.init()
