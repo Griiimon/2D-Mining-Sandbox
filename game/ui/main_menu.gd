0472f940ecda09e25aac9d96e7780c2b5f8dae1f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-16 14:41:19 +0200

    character list

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -1,8 +1,20 @@
 extends CanvasLayer
 
 
+@onready var character_item_list = %"Character ItemList"
+
+
+
 func _ready():
 	get_tree().paused= false
+	populate_lists()
+
+
+func populate_lists():
+	for character_scene in DataManager.characters:
+		character_item_list.add_item(character_scene.resource_path.rsplit("/")[-1].trim_suffix(".tscn").capitalize())
+
+	character_item_list.select(0)
 
 
 func _on_close_button_pressed():
