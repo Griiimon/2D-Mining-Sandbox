13cecb1aab516130b4013da4c1a9fef5c397f361
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-16 15:54:12 +0200

    update

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 
 @onready var character_item_list = %"Character ItemList"
+@onready var game_mode_item_list = %"Game Mode ItemList"
 
 
 
@@ -12,10 +13,25 @@ func _ready():
 
 func populate_lists():
 	for character_scene in DataManager.characters:
-		character_item_list.add_item(character_scene.resource_path.rsplit("/")[-1].trim_suffix(".tscn").capitalize())
+		character_item_list.add_item(get_scene_name(character_scene))
 
 	character_item_list.select(0)
+
+	for scenario in DataManager.builtin_scenarios + DataManager.scenarios:
+		game_mode_item_list.add_item(get_scene_name(scenario))
+
+	game_mode_item_list.select(0)
+
+
+static func get_scene_name(scene: PackedScene)-> String:
+	return scene.resource_path.rsplit("/")[-1].trim_suffix(".tscn").capitalize()
 
 
 func _on_close_button_pressed():
 	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	GameManager.character= DataManager.characters[character_item_list.get_selected_items()[0]]
+	var scenarios: Array[PackedScene]= DataManager.builtin_scenarios + DataManager.scenarios
+	GameManager.run_game(scenarios[game_mode_item_list.get_selected_items()[0]])
