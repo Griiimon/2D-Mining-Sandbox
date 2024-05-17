5bad5543c915eb75d6097518c14ab7b0f4f54b4b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-17 17:22:36 +0200

    Update sound_library.gd

--- game/audio/sound_library.gd
+++ game/audio/sound_library.gd
@@ -2,4 +2,12 @@ class_name SoundLibrary
 extends Resource
 
 
-@export var library: Dictionary
+@export var items: Array[SoundLibraryItem]
+
+var library: Dictionary
+
+
+
+func build():
+	for item in items:
+		library[item.key]= item.sound
