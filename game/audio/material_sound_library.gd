5a34e19b632b74e22a641957a5cc424645017376
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-17 17:22:20 +0200

    Update material_sound_library.gd

--- game/audio/material_sound_library.gd
+++ game/audio/material_sound_library.gd
@@ -20,10 +20,11 @@ static func get_key(type1: Type, type2: Type)-> String:
 
 func build():
 	for key in sound_paths.keys():
-		var arr= sound_paths[key]
+		var arr= sound_paths[key].split("\n")
 		if arr:
 			sounds[key]= []
-			for sound_file in arr:
-				assert(FileAccess.file_exists(sound_file))
+			for sound_file: String in arr:
+				sound_file= sound_file.strip_escapes()
+				assert(FileAccess.file_exists(sound_file), "Sound file %s doesnt exist" % [sound_file])
 				sounds[key].append(load(sound_file))
 
