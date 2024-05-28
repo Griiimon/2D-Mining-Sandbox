4e72432713a7e8cbe0b6feea8315e77106059b5d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-28 10:48:57 +0200

    ignore_height toggle

--- game/world/terrain generator/terrain_generator_instruction.gd
+++ game/world/terrain generator/terrain_generator_instruction.gd
@@ -4,13 +4,22 @@ extends Resource
 @export var min_height: int= 0
 @export var max_height: int= 1024
 @export var distribution: TerrainBlockDistribution
+@export var ignore_height: bool= false
 
 
-func initialize():
+var generator: TerrainGenerator
+var instruction: TerrainGeneratorInstruction
+
+
+
+func initialize(_generator: TerrainGenerator):
+	generator= _generator
 	distribution.initialize()
 
 
 func get_block(pos: Vector2i)-> Block:
 	if pos.y < min_height or pos.y > max_height:
+		return null
+	if not ignore_height and pos.y < generator.get_height(pos.x):
 		return null
 	return distribution.get_block(pos)
