0f47a9b5d381f442109a94d3a5dba6befbb52066
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-28 08:50:31 +0200

    flowing block utilities

--- game/blocks/fluid_library.gd
+++ game/blocks/fluid_library.gd
@@ -9,7 +9,7 @@ var block_to_fluid:= {}
 
 func build():
 	for fluid in fluids:
-		for block in fluid.blocks:
+		for block in fluid.blocks + fluid.flowing_blocks:
 			block_to_fluid[block]= fluid
 
 
@@ -37,6 +37,12 @@ func get_split_block(block: FluidBlock, depth: int= 1)-> FluidBlock:
 	if block.fill_ratio == FluidBlock.FillRatio.FULL:
 		depth+= 1
 	return get_lower_fluid_block(block, depth)
+
+
+func get_flowing_block(block: FluidBlock)-> FluidBlock:
+	if block.is_flowing: return block
+	var fluid: FluidLibraryItem= get_fluid(block)
+	return fluid.flowing_blocks[int(block.fill_ratio) - 1]
 
 
 func get_fluid(block: Block)-> FluidLibraryItem:
