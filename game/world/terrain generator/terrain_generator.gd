a185dd4c211b614e02d3869c282c6006e0cb4663
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-28 10:48:37 +0200

    add height noise

--- game/world/terrain generator/terrain_generator.gd
+++ game/world/terrain generator/terrain_generator.gd
@@ -2,12 +2,14 @@ class_name TerrainGenerator
 extends Resource
 
 @export var instructions: Array[TerrainGeneratorInstruction]
+@export var height_noise: FastNoiseLite
+@export var height_scale: float= 10.0
 
 
 
 func initialize():
 	for instruction in instructions:
-		instruction.initialize()
+		instruction.initialize(self)
 
 
 func get_block_id(pos: Vector2i)-> int:
@@ -19,3 +21,8 @@ func get_block_id(pos: Vector2i)-> int:
 
 	if not block or block.is_air: return -1
 	return DataManager.get_block_id(block)
+
+
+func get_height(x: int)-> int:
+	if not height_noise: return 999999
+	return height_noise.get_noise_1d(x) * height_scale
