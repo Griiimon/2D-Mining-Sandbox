2b00d3a8e39ea1bae84aa39853214f9929f6e48b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-29 11:40:39 +0200

    create boat

---
+++ game/vehicles/boat/boat.gd
@@ -0,0 +1,20 @@
+class_name Boat
+extends Vehicle
+
+@export var lift: float= 10.0
+
+@export var air_damp: float= 1.0
+@export var water_damp: float= 3.0
+
+@onready var tile_detector: TileDetector = $"Tile Detector"
+
+
+func movement(delta: float):
+	if not is_on_floor() and not tile_detector.is_in_fluid():
+		velocity.y += gravity * delta
+		velocity.y*= (1 - delta * air_damp) 
+	elif tile_detector.is_in_fluid():
+		velocity.y -= lift * delta
+		velocity.y*= (1 - delta * water_damp) 
+		
+	move_and_slide()
