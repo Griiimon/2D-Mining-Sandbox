2f554a50178d622824137e4111cfba97ce3c1eef
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-01 01:49:48 +0200

    add conditions

--- game/world/components/mob spawner/mob_spawner_definition.gd
+++ game/world/components/mob spawner/mob_spawner_definition.gd
@@ -8,6 +8,9 @@ enum Faction { FRIENDLY, NEUTRAL, HOSTILE }
 @export var empty_radius: int= 3
 @export var min_distance_to_same: int= 20
 @export var min_distance_to_other: int= 10
+@export var only_underground: bool= false
+@export var only_overworld: bool= false
+@export var only_in_fluid: bool= false
 
 
 
@@ -15,7 +18,20 @@ func can_spawn(tile: Vector2i, world: World)-> bool:
 	if not Utils.chance100(probability):
 		return false
 	
-	if not world.is_air_at_rect(Rect2i(tile - Vector2i.ONE * empty_radius, Vector2i.ONE * (empty_radius * 2 + 1))):
+	var underground= world.has_block_above(tile)
+	
+	if only_underground and not underground:
+		return false
+	
+	if only_overworld and underground:
+		return false
+	
+	var empty_rect:= Rect2i(tile - Vector2i.ONE * empty_radius, Vector2i.ONE * (empty_radius * 2 + 1))
+
+	if only_in_fluid:
+		return world.is_fluid_at_rect(empty_rect)
+	
+	if not world.is_air_at_rect(empty_rect):
 		return false
 	
 	return true
