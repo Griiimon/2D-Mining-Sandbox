659ce1b10baa94557a6bba59c3d65edaed1790ff
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-31 02:10:48 +0200

    Update water_row_state.gd

--- game/ui/admin/water_row_state.gd
+++ game/ui/admin/water_row_state.gd
@@ -6,18 +6,21 @@ extends StateMachineState
 
 func on_unhandled_input(event: InputEvent):
 	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			if Global.game and Global.game.world:
-				var world: World= Global.game.world
-				#var tile: Vector2i= world.local_to_map(event.position)
-				var tile: Vector2i= world.local_to_map(world.get_global_mouse_position())
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				if Global.game and Global.game.world:
+					var world: World= Global.game.world
+					var tile: Vector2i= world.local_to_map(world.get_global_mouse_position())
 
-				var x:= 0
-				while world.is_air_at(Vector2i(x, tile.y)):
-					world.set_block(water_block, Vector2i(x, tile.y))
-					x+= 1
-				
-				x= -1
-				while world.is_air_at(Vector2i(x, tile.y)):
-					world.set_block(water_block, Vector2i(x, tile.y))
-					x-= 1
+					var x:= 0
+					while world.is_air_at(Vector2i(x, tile.y)):
+						world.set_block(water_block, Vector2i(x, tile.y))
+						x+= 1
+					
+					x= -1
+					while world.is_air_at(Vector2i(x, tile.y)):
+						world.set_block(water_block, Vector2i(x, tile.y))
+						x-= 1
+			
+			elif event.button_index == MOUSE_BUTTON_RIGHT:
+				cancel()
