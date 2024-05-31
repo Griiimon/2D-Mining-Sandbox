e6518474828d7ff13bfef21f0829bbd069f80747
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-01 00:40:10 +0200

    spawn mob

--- game/ui/admin/admin_state_machine.gd
+++ game/ui/admin/admin_state_machine.gd
@@ -4,6 +4,7 @@ extends FiniteStateMachine
 
 @onready var water_row: StateMachineState = $"Water Row"
 @onready var spawn_scene: StateMachineState = $"Spawn Scene"
+@onready var spawn_mob: StateMachineState = $"Spawn Mob"
 
 
 func _on_water_row_button_pressed():
@@ -12,3 +13,7 @@ func _on_water_row_button_pressed():
 
 func _on_spawn_scene_button_pressed():
 	change_state(spawn_scene)
+
+
+func _on_spawn_mob_button_pressed():
+	change_state(spawn_mob)
