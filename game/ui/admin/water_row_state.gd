b83ab60cf9baa02690a5ddaf5674dea5ab82b75c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-31 14:35:05 +0200

    Update water_row_state.gd

--- game/ui/admin/water_row_state.gd
+++ game/ui/admin/water_row_state.gd
@@ -11,15 +11,14 @@ func on_unhandled_input(event: InputEvent):
 				if Global.game and Global.game.world:
 					var world: World= Global.game.world
 					var tile: Vector2i= world.local_to_map(world.get_global_mouse_position())
-
 					var x:= 0
-					while world.is_air_at(Vector2i(x, tile.y)):
-						world.set_block(water_block, Vector2i(x, tile.y))
+					while world.is_air_at(tile + Vector2i(x, 0)):
+						world.set_block(water_block, tile + Vector2i(x, 0))
 						x+= 1
 					
 					x= -1
-					while world.is_air_at(Vector2i(x, tile.y)):
-						world.set_block(water_block, Vector2i(x, tile.y))
+					while world.is_air_at(tile + Vector2i(x, 0)):
+						world.set_block(water_block, tile + Vector2i(x, 0))
 						x-= 1
 			
 			elif event.button_index == MOUSE_BUTTON_RIGHT:
