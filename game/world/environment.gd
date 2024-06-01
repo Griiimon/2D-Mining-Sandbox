b9f918a8eb74d1d080fa3de52565d6c3eddbb07f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-02 00:18:43 +0200

    dont auto initialize

--- game/world/environment.gd
+++ game/world/environment.gd
@@ -1,3 +1,4 @@
+class_name MyWorldEnvironment
 extends Node
 
 @export var cloud_scene: PackedScene
@@ -5,7 +6,7 @@ extends Node
 @onready var clouds = $Clouds
 
 
-func _ready():
+func init():
 	for i in 100:
 		var cloud: Cloud= cloud_scene.instantiate()
 		cloud.position= Vector2(randf_range(-5000, 5000), randf_range(-1000, -100))
