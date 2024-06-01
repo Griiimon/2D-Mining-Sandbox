9403fe956a9ddf0520ce64615771ea0d931fae8b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-01 16:20:25 +0200

    Create backgound.gd

---
+++ game/world/backgound.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var sky: ColorRect = $Sky
+@onready var stars: TextureRect = $Stars
+
+
+
+func _process(delta):
+	var y: float= get_viewport().get_camera_2d().global_position.y
+	var shader: ShaderMaterial= sky.material
+	shader.set_shader_parameter("y", y)
+	stars.modulate= lerp(Color.TRANSPARENT, Color.WHITE, clamp(pow(y / 5000.0, 2), 0.0, 1.0))
