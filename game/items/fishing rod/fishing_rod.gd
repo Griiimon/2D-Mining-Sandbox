8dfdb5279b3a7df16a883d4e4a062cc1968c823a
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-02 03:14:06 +0200

    shoot hook

--- game/items/fishing rod/fishing_rod.gd
+++ game/items/fishing rod/fishing_rod.gd
@@ -1,3 +1,20 @@
 extends HandItemObject
 
+@onready var line: Line2D = $Line
+@onready var hook: Line2D = $Hook
+@onready var hook_body: FishingRodHookBody = %"Hook body"
 
+
+
+func release_charge(total_charge: float, primary: bool):
+	if not primary: return
+	
+	hook_body.shoot(total_charge, transform.x)
+
+
+func _process(delta):
+	if not hook_body.top_level: return
+	
+	hook.global_position= hook_body.global_position
+	#line.points[-1]= hook.global_position - line.global_position
+	line.points[-1]= line.to_local(hook.global_position)
