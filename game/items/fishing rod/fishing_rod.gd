c3cb3f83a4120ce65073f7aa97d790b8268a7960
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-02 04:47:40 +0200

    Update fishing_rod.gd

--- game/items/fishing rod/fishing_rod.gd
+++ game/items/fishing rod/fishing_rod.gd
@@ -1,20 +1,38 @@
 extends HandItemObject
 
+@export var reel_in_speed: float= 500.0
+
 @onready var line: Line2D = $Line
 @onready var hook: Line2D = $Hook
 @onready var hook_body: FishingRodHookBody = %"Hook body"
+@onready var hook_orig_position = $"Hook Orig Position"
 
+
+var hook_tween: Tween
+var reel_in: bool= false
+
+
+
+func action(primary: bool):
+	if not primary:
+		reel_in= true
 
 
 func release_charge(total_charge: float, primary: bool):
 	if not primary: return
 	
-	hook_body.shoot(total_charge, transform.x)
+	hook_body.shoot(hook_orig_position.global_position, total_charge, (transform.x - transform.y).normalized())
 
 
 func _process(delta):
 	if not hook_body.top_level: return
+
+	if reel_in:
+		hook_body.position= hook_body.position.move_toward(hook_orig_position.global_position, reel_in_speed * delta)
+		if hook_body.position.distance_to(hook_orig_position.global_position) < 1:
+			reel_in= false
+			hook_body.top_level= false
+			hook_body.position= Vector2.ZERO
 	
 	hook.global_position= hook_body.global_position
-	#line.points[-1]= hook.global_position - line.global_position
 	line.points[-1]= line.to_local(hook.global_position)
